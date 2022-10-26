fd69508dbc6edc65d1a8aaf37bdfef75b227b0c3
Sergey <Sergey@localhost.localdomain>
2022-10-27 00:17:00 +0300

    Added verbose names for all fields in each model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField()
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
+    title = models.CharField(verbose_name='Название', max_length=200)
+    image = models.ImageField(verbose_name='Изображение', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание',)
+    title_en = models.CharField(verbose_name='Название на английском языке', max_length=200)
+    title_jp = models.CharField(verbose_name='Название на японском языке', max_length=200)
     previous_form = models.ForeignKey(
         'Pokemon',
+        verbose_name='Предыдущая ступень эволюции',
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
@@ -20,15 +21,15 @@ class Pokemon(models.Model):
         return self.title
 
 class PokemonEntity(models.Model):
-    latitude = models.FloatField(null=False)
-    longitude = models.FloatField(null=False)
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    latitude = models.FloatField(verbose_name='Широта', null=False)
+    longitude = models.FloatField(verbose_name='Долгота', null=False)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)
 
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
+    appeared_at = models.DateTimeField(verbose_name='Время появления', default=timezone.now)
+    disappeared_at = models.DateTimeField(verbose_name='Время исчезновения', default=timezone.now)
 
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    level = models.IntegerField(verbose_name='Уровень')
+    health = models.IntegerField(verbose_name='Здоровье')
+    strength = models.IntegerField(verbose_name='Сила')
+    defence = models.IntegerField(verbose_name='Защита')
+    stamina = models.IntegerField(verbose_name='Выносливость')
