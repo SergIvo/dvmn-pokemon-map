8254a970023f091224049b662d1b7947e7236469
Sergey <Sergey@localhost.localdomain>
2022-10-26 13:20:28 +0300

    Pokemons from db now shown on main page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,9 @@
 import folium
 import json
 
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, request
 from django.shortcuts import render
+from pokemon_entities.models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -40,11 +41,15 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
+        if pokemon.image:
+            pokemon_image_url = request.build_absolute_uri(pokemon.image.url)
+        else:
+            pokemon_image_url = None
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon_image_url,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
