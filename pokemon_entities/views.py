96604e1fdff916411e894462425f0c2f6d67c048
Sergey <Sergey@localhost.localdomain>
2022-10-29 23:23:23 +0300

    Removed nested loop

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -32,22 +32,22 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     current_datetime = timezone.localtime()
-    for pokemon in Pokemon.objects.all():
-        for pokemon_entity in pokemon.entities.filter(
-            appeared_at__lte=current_datetime,
-            disappeared_at__gte=current_datetime
-        ):
-            if pokemon.image:
-                add_pokemon(
-                    folium_map, pokemon_entity.latitude,
-                    pokemon_entity.longitude,
-                    request.build_absolute_uri(pokemon.image.url)
-                )
-            else:
-                add_pokemon(
-                    folium_map, pokemon_entity.latitude,
-                    pokemon_entity.longitude,
-                )
+    all_entities = PokemonEntity.objects.filter(
+        appeared_at__lte=current_datetime,
+        disappeared_at__gte=current_datetime
+    )
+    for pokemon_entity in all_entities:
+        if pokemon_entity.pokemon.image:
+            add_pokemon(
+                folium_map, pokemon_entity.latitude,
+                pokemon_entity.longitude,
+                request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+            )
+        else:
+            add_pokemon(
+                folium_map, pokemon_entity.latitude,
+                pokemon_entity.longitude,
+            )
 
     pokemons_on_page = []
     for pokemon in Pokemon.objects.all():
